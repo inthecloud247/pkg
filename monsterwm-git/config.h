65f68b08c6fd440206e82d4377be937842214bed
Eivind Uggedal <eivind@uggedal.com>
2012-07-24 17:37:54 +0200

    More prominent focused border.

--- monsterwm-git/config.h
+++ monsterwm-git/config.h
@@ -19,7 +19,7 @@
 #define FOLLOW_MOUSE    False     /* Focus the window the mouse just entered */
 #define CLICK_TO_FOCUS  False     /* Focus an unfocused window when clicked  */
 #define BORDER_WIDTH    2         /* window border width */
-#define FOCUS           "#eee8d5" /* focused window border color    */
+#define FOCUS           "#b58900" /* focused window border color    */
 #define UNFOCUS         "#fdf6e3" /* unfocused window border color  */
 #define MINWSZ          50        /* minimum window size in pixels  */
 #define DEFAULT_DESKTOP 0         /* the desktop to focus initially */
