99c02265a3063e439a175914f6b7defa10f8d228
Eivind Uggedal <eivind@uggedal.com>
2012-08-17 13:45:23 +0200

    Add pkg publish wrapper.

---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DB=uggedal.db.tar.gz
+REPO=/srv/http/pkg/x86_64
+
+find $(dirname $0) -maxdepth 1 -type d | while read d; do
+  find $d -maxdepth 1 -type f -name \*.pkg.tar.xz | sort -V | tail -n 1 | while read f; do
+  
+    name=$(basename $f)
+    if [ ! -e $REPO/$name ]; then
+      rm -f $REPO/$(echo $name | cut -d - -f 1)*.pkg.tar.xz
+      cp $f $REPO/$name
+      repo-add $REPO/$DB $REPO/$name
+    fi
+  done
+done
